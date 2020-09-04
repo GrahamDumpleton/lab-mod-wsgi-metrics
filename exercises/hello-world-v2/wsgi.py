e4f2f2073cae63c39e2fb928a3378d424c0414dc
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-09-04 22:33:37 +1000

    Add tags for hostname and process.

--- exercises/hello-world-v2/wsgi.py
+++ exercises/hello-world-v2/wsgi.py
@@ -1,3 +1,6 @@
+import socket
+import os
+
 import mod_wsgi
 
 from influxdb import InfluxDBClient
@@ -5,12 +8,21 @@ from datetime import datetime
 
 client = InfluxDBClient('localhost', 8086, 'wsgi', 'wsgi', 'wsgi')
 
+hostname = socket.gethostname()
+pid = os.getpid()
+
+process = f"{hostname}:{pid}"
+
 def event_handler(name, **kwargs):
     if name == 'request_finished':
         client.write_points([
             {
                 "measurement": "wsgi.requests",
                 "time": datetime.now().isoformat(),
+                "tags": {
+                    "hostname": hostname,
+                    "process": process
+                },
                 "fields": {
                     "application_time": kwargs["application_time"]
                 }
